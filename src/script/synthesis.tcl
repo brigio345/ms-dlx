a8cbbc54b43d2ba0b2f2b7d785bfe86c69f5e3b5
brigio345 <brigio345@gmail.com>
2020-09-09 14:09:04 +0200

    feat(synthesis): introduce synthesis without dual V-th
    Signed-off-by: brigio345 <brigio345@gmail.com>

--- src/script/synthesis.tcl
+++ src/script/synthesis.tcl
@@ -19,7 +19,7 @@ proc analyze_dir {dir {src_format "vhdl"}} {
 	}
 }
 
-# synthesize
+# synthesize_dual_vth
 #	* Argument(s):
 #		- top_entity: name of top level entity to be synthesized
 #		- time_constraint: maximum delay between inputs and outputs
@@ -27,7 +27,9 @@ proc analyze_dir {dir {src_format "vhdl"}} {
 #		- lvt_lib: low threshold voltage library
 #		- hvt_lib: high threshold voltage library
 #	* Result:
-#		- synthesized top_entity (written to $top_entity-YYMMDDHHmm-postsyn.v)
+#		Synthesize top_entity with dual V-th enabled
+#		- post-synthesis netlist (written to $top_entity-YYMMDDHHmm-postsyn.v)
+#		- sdc file (written to $top_entity.sdc)
 #		- timing report (written to $top_entity-YYMMDDHHmm-timing.rpt)
 #		- power report (written to $top_entity-YYMMDDHHmm-power.rpt)
 #		- area report (written to $top_entity-YYMMDDHHmm-area.rpt)
@@ -35,7 +37,7 @@ proc analyze_dir {dir {src_format "vhdl"}} {
 #		- clock gating report (written to $top_entity-YYMMDDHHmm-gating.rpt)
 #	* Return:
 #		none
-proc synthesize {{top_entity "dlx"} {time_constraint 1.0005}
+proc synthesize_dual_vth {{top_entity "dlx"} {time_constraint 2}
 			{lvt_lib "CORE65LPLVT"} {hvt_lib "CORE65LPHVT"}} {
 	elaborate $top_entity
 
@@ -55,10 +57,52 @@ proc synthesize {{top_entity "dlx"} {time_constraint 1.0005}
 	set name "$top_entity-[clock format [clock seconds] -format %Y%m%d%H%M]"
 
 	write -format verilog -hierarchy -output "$name-postsyn.v"
+	write_sdc "$name.sdc"
 	report_timing > "$name-timing.rpt"
 	report_power > "$name-power.rpt"
 	report_area > "$name-area.rpt"
 	report_threshold_voltage_group > "$name-threshold-rpt"
+	report_clock_gating > "$name-gating.rpt"
+}
+
+# synthesize
+#	* Argument(s):
+#		- top_entity: name of top level entity to be synthesized
+#		- time_constraint: maximum delay between inputs and outputs
+#			and clock period
+#		- wire_load_model: name of the wire load model
+#	* Result:
+#		Synthesize top_entity
+#		- post-synthesis netlist (written to $top_entity-YYMMDDHHmm-postsyn.v)
+#		- sdc file (written to $top_entity.sdc)
+#		- timing report (written to $top_entity-YYMMDDHHmm-timing.rpt)
+#		- power report (written to $top_entity-YYMMDDHHmm-power.rpt)
+#		- area report (written to $top_entity-YYMMDDHHmm-area.rpt)
+#		- clock gating report (written to $top_entity-YYMMDDHHmm-gating.rpt)
+#	* Return:
+#		none
+proc synthesize {{top_entity "dlx"} {time_constraint 2} {wire_load_model "5K_hvratio_1_4"}} {
+	elaborate $top_entity
+
+	# setup timing constraints
+	create_clock -period $time_constraint I_CLK
+	set_max_delay -from [all_inputs] -to [all_outputs] $time_constraint
+
+	# setup working conditions
+	set_wire_load_model -name $wire_load_model
+
+	ungroup -all -flatten
+	
+	compile_ultra -timing_high_effort_script -gate_clock -retime
+
+	# write outputs
+	set name "$top_entity-[clock format [clock seconds] -format %Y%m%d%H%M]"
+
+	write -format verilog -hierarchy -output "$name-postsyn.v"
+	write_sdc "$name.sdc"
+	report_timing > "$name-timing.rpt"
+	report_power > "$name-power.rpt"
+	report_area > "$name-area.rpt"
 	report_clock_gating > "$name-gating.rpt"
 }
 
